e870b4208139809e8ffedfd9f9d798772e201616
muhos <muh.osama25@gmail.com>
2026-04-30 13:08:12 +0200

    encapped colors in namespace

--- include/cuarena/color.hpp
+++ include/cuarena/color.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+namespace cuarena {
+
 #define CNORMAL   "\x1B[0m"
 #define CERROR    "\x1B[1;31m"
 #define CWARNING  "\x1B[1;33m"
 #define CINFO     "\x1B[1;35m"
 #define CDEBUG    "\x1B[0;37m"
+
+}
